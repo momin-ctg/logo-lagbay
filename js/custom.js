7516849298761235814f2c339f3535c789db21e3
CoDesign333-4 <faisalhmohd@live.com>
2018-05-31 15:00:54 +0600

    ns

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/**
+ * Mobile Nav
+ * Trigger
+ */
+
+    function openNav() {
+        document.getElementById("myNav").style.height = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.height = "0";
+    }
+
+    // end
+
